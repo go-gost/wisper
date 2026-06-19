96ed1f2bd1882abfee4f6e02ed8d3c65fbec795a
ginuerzh <ginuerzh@gmail.com>
2026-06-19 09:54:35 +0800

    chore(version): default to 0.0.0-dev for non-release builds

--- version/version.go
+++ version/version.go
@@ -1,4 +1,7 @@
 package version
 
-// Version is the current version of Wisper.
-var Version = "0.1.0"
+// Version is the current version of Wisper. It is overridden at release build
+// time via -ldflags "-X ...version.Version=<tag>" (see .goreleaser.yaml and
+// .github/workflows/release.yml). The default is a dev marker so an unbuilt
+// or `go run` binary is obviously not a release.
+var Version = "0.0.0-dev"
