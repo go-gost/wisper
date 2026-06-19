16d3391adfbfc0726175110520f31309f99ce2a6
ginuerzh <ginuerzh@gmail.com>
2026-06-19 10:07:14 +0800

    chore: use 0.0.0-dev placeholders for local Tauri/Android builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.0.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
